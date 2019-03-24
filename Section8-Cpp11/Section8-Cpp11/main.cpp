1adf3fdd51f5ebadbe5946954ee4de43c608d2b7
Luciano Almeida <passos.luciano@outlook.com>
2019-03-24 18:34:22 -0300

    S08 L41 Range-Based Loops

--- Section8-Cpp11/Section8-Cpp11/main.cpp
+++ Section8-Cpp11/Section8-Cpp11/main.cpp
@@ -12,7 +12,6 @@ auto test(T value, S other) -> decltype(value + other) {
     return value + other;
 }
 
-
 int main(int argc, const char * argv[]) {
     // S08 L39 Decltype, typeid and name mangling.
     std::string s;
@@ -26,6 +25,12 @@ int main(int argc, const char * argv[]) {
     
     std::cout << test(i, 5) << std::endl;
     
+    // S08 L41 Range-Based Loops
+    auto texts = {"1", "3", "2"};
+    
+    for (auto text : texts) {
+        std::cout << text << std::endl;
+    }
     
     return 0;
 }
