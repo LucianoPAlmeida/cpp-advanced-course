c9d1dce150184a116035ed2bb07a72d85c6ebfe0
Luciano Almeida <passos.luciano@outlook.com>
2019-03-24 16:03:43 -0300

    Lession 35: Function pointers.

--- Templates/Templates/main.cpp
+++ Templates/Templates/main.cpp
@@ -7,17 +7,65 @@
 //
 
 #include <iostream>
+#include <algorithm>
+#include <vector>
+
 template<class T>
 class Test {
 public:
     T i;
+    void print() const {
+        std::cout << "T: " << this->i << std::endl;
+    }
 };
 
+template <class T>
+void print(T t) {
+    std::cout << t << std::endl;
+}
+
+void test() {
+    std::cout << "Testing" << std::endl;
+}
+
+bool matches(std::string s)  {
+    return s.size() != 3;
+}
+template <class Iterator, class T>
+int count_if(Iterator begin, Iterator end, bool (*matches)(T))
+{
+   int count = 0;
+    for (; begin != end; ++begin)
+      if (matches(*begin))
+        ++count;
+    return count;
+}
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
     Test<int> t;
+    t.i = 2;
+    t.print();
+    
+    print(1);
     
     
+    // Function pointers
+    void (*pF)() = &test;
+    void (*tpF)(int) = &print;
+    
+    pF();
+    tpF(56);
+    
+    std::vector<std::string> v;
+    v.push_back("two");
+    v.push_back("three");
+    v.push_back("one");
+
+//    auto count = count_if(v.begin(), v.end(), matches); // Our custom count_if
+    auto count = std::count_if(v.begin(), v.end(), matches);
+
+    std::cout << "Count: " << count << std::endl;
     
     return 0;
 }
