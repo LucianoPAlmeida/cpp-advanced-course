b1afd7efe29fdd836176fb4307799bd2c4300cf0
Luciano Almeida <passos.luciano@outlook.com>
2020-04-19 20:48:34 -0300

    Cpp Unions

---
+++ Unions/Unions/main.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  Unions
+//
+//  Created by Luciano Almeida on 19/04/20.
+//  Copyright © 2020 Luciano Almeida. All rights reserved.
+//
+
+#include <iostream>
+
+
+struct Vector2 {
+  float x, y;
+};
+
+union Vector4 {
+  struct {
+    float x, y, z, w;
+  };
+  struct {
+    Vector2 a, b;
+  };
+};
+
+void dump(Vector2 v) {
+  std::cout << "{x: "<< v.x << ", y: "<< v.y << "}" << std::endl;
+}
+
+
+int main(int argc, const char * argv[]) {
+  // Memory area is shared.
+  Vector4 v = {1.0 , 2.0 , 3.0, 4.0};
+  
+  dump(v.a);
+  dump(v.b);
+  
+  v.w = 10.0;
+  dump(v.a);
+  dump(v.b);
+
+  
+  return 0;
+}
